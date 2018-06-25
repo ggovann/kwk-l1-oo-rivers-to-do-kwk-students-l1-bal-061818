09c56baaa1c5860e7231078478e4219bcb0a4103
Grace Govan <ggovan11@outlook.com>
2018-06-25 15:46:28 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,22 @@
-# river.rb
+class River 
+  def initialize(name,location, countries, discharge)
+    @name = name
+    @location = location
+    @countries = countries
+    
+    @discharge = discharge
+  end  
+  
+  def flood
+    @flood += discharge * 1.3 
+  end
+
+  def dry_up
+   @dry_up +=  discharge * 0.5
+  end
+ end
+ 
+The_Nile = River.new("The Nile", "Egypt", "Africa")
+The_Mississippi = River.new("The Mississippi", "Missouri", "")
+
+ 
